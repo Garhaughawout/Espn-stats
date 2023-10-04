399374acfce2bcbc26b2edc88766a98c2c208053
Garhaughawout <108907661+Garhaughawout@users.noreply.github.com>
2023-10-04 13:57:01 -0600

    Final Footer update
    In this commit I've completed the Footer and the clone is basically done. Although finished with the basic clone. I will be updating the page to have more components load when certain buttons are interacted with.

--- espn-stats/src/Components/Footer.js
+++ espn-stats/src/Components/Footer.js
@@ -10,19 +10,31 @@ export default function Footer() {
                     <img src={espnLogo}/>
                 </div>
                 <div className="footer-text">
-                    <button>Terms of Use </button>
-                    <p className="breakup-line">|</p>
-                    <button>Privacy Policy</button>
-                    <p className="breakup-line">|</p>
-                    <button>Your US State Privacy Rights</button>
-                    <p className="breakup-line">|</p>
-                    <button>Children's Online Privacy Policy</button>
-                    <p className="breakup-line">|</p>
-                    <button>Interest-Based Ads</button>
-                    <p className="breakup-line">|</p>
-                    <button>About Nielson Measurement</button>
-                    <p className="breakup-line">|</p>
-                    <button></button>
+                    <div className="footer-text-section">
+                        <button>Terms of Use </button>
+                        <p className="breakup-line"></p>
+                        <button>Privacy Policy</button>
+                        <p className="breakup-line"></p>
+                        <button>Your US State Privacy Rights</button>
+                        <p className="breakup-line"></p>
+                        <button>Children's Online Privacy Policy</button>
+                        <p className="breakup-line"></p>
+                        <button>Interest-Based Ads</button>
+                        <p className="breakup-line"></p>
+                        <button>About Nielson Measurement</button>
+                    </div>
+                    <div className="footer-text-section">
+                        <button>Do Not Sell or Share My Personal Information</button>
+                        <p className="breakup-line"></p>
+                        <button>Contact Us</button>
+                        <p className="breakup-line"></p>
+                        <button>Disney Ad Sales Site</button>
+                        <p className="breakup-line"></p>
+                        <button>Work for ESPN</button>
+                    </div>
+                    <div className="footer-text-section">
+                        <p className="copy-right-text">Copyright: &#xA9; ESPN Enterpries, Inc. All rights reserved.</p>
+                    </div>
                 </div>
             </div>
         </div>
